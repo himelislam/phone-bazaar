41d11b2972dd428cebd16b99ab5db4cc9db0f4dc
himelislam <himel7100@gmail.com>
2022-03-01 03:13:46 +0600

    ui all featured added

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const searchBtn = () =>{
 
 const displayPhones = (phones) => {
     const resultFieldContainer = document.getElementById('result-field')
+    resultFieldContainer.textContent = ''
     phones.forEach(phone => {
         // console.log(phone.image)
         const div = document.createElement('div')
@@ -36,7 +37,8 @@ const getPhoneId = (id) =>{
 }
 
 const displayDetails =(phone) =>{
-    console.log(phone)
+    console.log(phone.others)
+    
     document.getElementById('details-field').innerHTML= `
     <div class="card mb-3">
                 <div class="row g-0">
@@ -48,14 +50,40 @@ const displayDetails =(phone) =>{
                       <h2 class="card-title">${phone.name}</h2>
                       <h6 class="card-title text-muted">${phone.releaseDate ? phone.releaseDate: 'Release Date Not Found'}</h6>
                       <h3 class="card-title text-muted">Main Features:</h3>
-                      <p class="card-text">Chipset: ${phone.mainFeatures.chipSet}</p>
-                      <p class="card-text">Display Size: ${phone.mainFeatures.displaySize}</p>
-                      <p class="card-text">Memory: ${phone.mainFeatures.memory}</p>
-                      <p class="card-text">Storage: ${phone.mainFeatures.storage}</p>
-                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                      <p class="">Chipset: ${phone.mainFeatures.chipSet}</p>
+                      <p class="">Display Size: ${phone.mainFeatures.displaySize}</p>
+                      <p class="">Memory: ${phone.mainFeatures.memory}</p>
+                      <p class="">Storage: ${phone.mainFeatures.storage}</p>
+                      <p class=""><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <h3 class="text-muted">Other Features</h3>
+                            <p>Bluetooth: ${phone.others.Bluetooth} </p>
+                            <p>GPS: ${phone.others.GPS}</p>
+                            <p>NFC: ${phone.others.NFC}</p>
+                            <p>Radio: ${phone.others.Radio}</p>
+                            <p>USB: ${phone.others.USB}</p>
+                            <p>WLAN: ${phone.others.WLAN}</p>
+                        </div>
+                        <div id="sensors" class="col-lg-6"> 
+                            <h3 class="card-title text-muted">Sensors:</h3>
+                        </div>
+                    </div>
+
                     </div>
                   </div>
                 </div>
               </div>
     `;
+    getSensorData(phone.mainFeatures.sensors)
+}
+
+const getSensorData=(sensors)=>{
+    const sensorContainer = document.getElementById('sensors');
+    sensors.forEach(sensor =>{
+        console.log(sensor)
+        const p = document.createElement('p')
+        p.innerText=sensor;
+        sensorContainer.appendChild(p)
+    })
 }
